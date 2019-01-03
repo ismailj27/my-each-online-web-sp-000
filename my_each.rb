40f355d5e6cf3e0fea72c302f1eaf86a14a1518f
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-03 17:34:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each (array) # put argument(s) here
   # code here
   idx = 0
   while idx < array.length
-    yield array[idx]
+    yield(array[idx])
     array = array.pop
     idx = idx + 1
   end
